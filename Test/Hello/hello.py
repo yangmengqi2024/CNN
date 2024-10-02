4f142b778b4a38a7679e57191aca425f891a7dd9
unknown <yangmengqi2024@hotmail.com>
2024-10-03 01:55:22 +0300

    Initial commit

---
+++ Test/Hello/hello.py
@@ -0,0 +1,15 @@
+# 编写python程序
+# 删除当前行 shift + delete
+# 复制 ctrl + D
+# 添加删除注释 ctrl + /
+# 快速格式化ctrl + alt + L
+# 快速运行，默认ctrl + shift + f10/ shift + f10 自定义
+# 查看类的层级关系 ctrl +H
+# 查找ctrl + f
+# 替换ctrl + r
+print("hi, python")
+print("hi, python")
+
+print("hi, python")
+print("hi, python")
+print("hi, python")
---
+++ Test/Hello/hi.py
@@ -0,0 +1,4 @@
+# @version  : 1.0
+# @Author   : Yang Mengqi
+# @File     : hi.py
+# @Time     : 2024/3/12 22:05
